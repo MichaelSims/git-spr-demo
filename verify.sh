fc5f3cd113abc4324d5a8bd968cc33c4777fb327
Michael Sims <michael.h.sims@gmail.com>
2023-11-17 19:17:22 -0600

    Enhance verify script

--- verify.sh
+++ verify.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env bash
 
-true
+set -x
+set -e
+
+verifyDelay=$(git show --pretty=full | awk '/^    verify-delay:/ { print $2 }')
+verifyResultAfterDelay=$(git show --pretty=full | awk '/^    verify-result-after-delay:/ { print $2 }')
+
+if [[ "$verifyDelay" -gt 0 ]]; then
+  echo "Delaying $verifyDelay seconds"
+  sleep "$verifyDelay"
+fi
+
+if [[ "$verifyResultAfterDelay" ]]; then
+  echo "Exiting with code $verifyResultAfterDelay"
+  exit "$verifyResultAfterDelay"
+else
+    exit 255
+fi
